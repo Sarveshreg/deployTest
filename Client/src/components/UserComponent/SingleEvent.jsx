3891363446c3c434a9b012d2372cbab25429c7f2
Sarveshreg <sarveshregmi1@gmail.com>
2024-04-03 16:07:38 -0500

    updated the profile page. User is now able to view events created by them, Delet the events, send RSVP for an event and also cancel an rsvp for an event

--- Client/src/components/UserComponent/SingleEvent.jsx
+++ Client/src/components/UserComponent/SingleEvent.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useEffect,useState } from 'react';
+import { useEffect,useState,useRef } from 'react';
 import { useParams } from 'react-router-dom';
 import {useSelector,useDispatch} from "react-redux"
 import { useNavigate } from 'react-router-dom';
@@ -10,11 +10,16 @@ function SingleEvent() {
     let User_fname=useSelector((state)=>state.auth.firstname);
     let token=useSelector((state)=>state.auth.token);
     let[eventDetail,setEventDetail]=useState({});
-    let[userComment,setUserComment]=useState(null);
-    let commentError=false;
+    //let[userComment,setUserComment]=useState(null);
+    let c=useRef();
+    let [commentError,setCommentError]=useState(false);
+    let [rsvpError,setRsvpError]=useState(false);
     let navigate = useNavigate();
+    let[RsvpDisable,setRsvpDisable]=useState(false)
+
 
     useEffect(()=>{
+      RsvpDisable=false;
       let detail=async()=>{
         let response=await fetch(`http://localhost:3000/api/events/${id}`,{
           method:"GET",
@@ -24,17 +29,77 @@ function SingleEvent() {
           setEventDetail(data);
           console.log(setEventDetail);
           console.log(data);
+          if(data.CreatorId==userId){
+            console.log("same creator")
+            setRsvpDisable(false)
+          }
+          else if(data.RSVPUsers.length>=data.MaximumAttendies){
+            setRsvpDisable(true)    //disable the RSVP button if enough users have RSVP'ed
+          }
+          let c= data.RSVPUsers.filter((user)=>user.id==userId)
+          if(c){
+            console.log("current user has already rsvp");
+            setRsvpDisable(true);
+          }
         }
       };
       detail();
-    },[userComment])
+      
+    },[commentError,RsvpDisable])
 
     let sendRsvp= async ()=>{
+      setRsvpError(true);
       console.log("rsvp clicked by",userId);
+      try {
+        let response= await fetch(`http://localhost:3000/api/rsvp/${id}`,{
+        method:"POST",
+        headers:{
+          "Content-Type" : "application/json",
+          "Authorization" : `Bearer ${token}`     //provide the token 
+        },
+        body:JSON.stringify({
+          User_fname,
+        })
+        })
+        let result=await response.json();
+        if(result.eventID==id){
+          setRsvpError(false);
+        }
+        else{
+          //do nothing
+        }
+        
+      } catch (error) {
+          console.error(error);
+      }
+}
+let cancelRsvp= async ()=>{
+  console.log("rsvp cancelled by",userId);
+  try {
+    let response= await fetch(`http://localhost:3000/api/rsvp/${id}`,{
+    method:"DELETE",
+    headers:{
+      "Content-Type" : "application/json",
+      "Authorization" : `Bearer ${token}`     //provide the token 
+    },
+    })
+    let result=await response.json();
+    console.log("result of cancel",result)
+    // if(result.eventID==id){
+    //   setRsvpError(false);
+    // }
+    // else{
+    //   //do nothing
+    // }
+    
+  } catch (error) {
+      console.error(error);
+  }
 }
 
     let postComment=async()=>{
-      console.log("posting comment",userComment," by ",userId, " ",User_fname );
+      setCommentError(true);
+      console.log("posting comment",c," by ",userId, " ",User_fname );
       try {
         let response= await fetch(`http://localhost:3000/api/events/${id}/comment`,{
         method:"POST",
@@ -43,16 +108,17 @@ function SingleEvent() {
           "Authorization" : `Bearer ${token}`     //provide the token 
         },
         body:JSON.stringify({
-          Comment:userComment,
+          Comment:c.current.value,
           User_fname,
         })
         })
         let result=await response.json();
         if(result.id){
-          setUserComment("")
+          c.current.value="";
+          setCommentError(false);
         }
         else{
-          commentError=true
+          //do nothing
         }
 
       } catch (error) {
@@ -92,18 +158,26 @@ function SingleEvent() {
               <span> {eventDetail.Street}</span>
               <p><span>{eventDetail.State} {eventDetail.ZipCode}</span></p>
           </span>
-          <p><strong>Date:</strong> {eventDetail.Details}</p>
-          <p><strong>Time:</strong> {eventDetail.Details}</p>
-          <p><strong>Maximum Attendees:</strong> {eventDetail.Details}</p>
+          <p><strong>Date and Time:</strong> {eventDetail.Date}</p>
+          <p><strong>Maximum Attendees:</strong> {eventDetail.MaximumAttendies}</p>
           <p><strong>RSVP:</strong> Required</p>
-          {token && <button onClick={(e)=>{sendRsvp()}}>I would like to attend</button>}
+          {/* {token && 
+          <span><button disabled={RsvpDisable} onClick={(e)=>{sendRsvp()}}>I would like to attend</button> {rsvpError && <span>Unable to RSVP!</span>}</span>
+          } */}
+          {token && <span>
+              {!RsvpDisable && <button onClick={(e)=>{cancelRsvp()}}>Cancel RSVP</button>}
+              {RsvpDisable && <button onClick={(e)=>{sendRsvp()}}> RSVP</button>}
+            </span>}
 
           <div>
-            <div><strong>People attending this event:</strong></div>
+            <div><strong>People attending this event ({eventDetail.RSVPUsers && <span>{eventDetail.RSVPUsers.length}</span>}):</strong></div>
             <span>
                   {eventDetail.RSVPUsers && eventDetail.RSVPUsers.map(user=>
                     <i key={User_fname}>
-                      <span> {user.User_fname} </span>               
+                      <div>
+                        <img src="https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg" alt="profile pic" height={50} width={50} />
+                      <div> {user.User_fname} </div> 
+                      </div>              
                     </i>
                   )}
             </span>
@@ -113,7 +187,7 @@ function SingleEvent() {
             <h5>Comments:</h5>
               {token &&
                 <span>
-                <textarea rows={3} cols={30} value={userComment} onChange={(e)=>setUserComment(e.target.value)}></textarea>
+                <textarea rows={3} cols={30} ref={c} ></textarea>
                 <button onClick={()=>postComment()}>Post</button>
                 <span>{commentError && <p>Unable to post comment</p>}</span>
               </span>
